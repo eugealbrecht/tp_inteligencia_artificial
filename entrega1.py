4219e36d5a488e62cdc8ae069eb214bb26d401b8
LAPTOP-5PUCK7BU\euge_ <albrechteuge@gmail.com>
2020-10-24 11:58:57 -0300

    no message

--- entrega1.py
+++ entrega1.py
@@ -9,3 +9,24 @@ astar,
 
 from simpleai.search.viewers import WebViewer, BaseViewer
 
+INITIAL_STATE = ()
+
+class MercadoArtificial(SearchProblem):
+
+    def is_goal(self, state):
+        pass
+
+    def cost(self, state1, action, state2):
+        pass
+
+    def actions(self, state):
+        pass
+
+    def result(self, state, action):
+        pass
+
+    def heuristic(self, state):
+        pass
+
+if __name__ == '__main__':
+    problema = MercadoArtificial(INITIAL_STATE)
